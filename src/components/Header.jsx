b09c54c2310058586d901085a2b12ee3458601ce
Brice789 <kireche.brice@gmail.com>
2023-02-07 19:02:40 +0100

    --

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Logo from "./img/logo.png"
 import {MdShoppingBasket } from 'react-icons/md'
+import { useHover } from 'react-laag';
 
 function Header() {
   return (
     <header className='fixeed z-50 w-Screen bg-slate-300 p-6 px-16'>
         {/* { desktop & tablet } */}
         <div className="hidden md:flex w-full h-full">
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 ">
                 <img src = {Logo} className="w-10 object-cover" alt = "logo"/>
                 <p className="text-headingColor text-xl font-blod"> City </p>
             </div>
@@ -27,9 +28,13 @@ function Header() {
                 </li>
             </ul>
 
-            {/* <div className="relativeative">
-             <MdShoppingBasket/>
-            </div> */}
+            <div className="relativeative">
+             <MdShoppingBasket className='text-textColor text-2xl ml-8 top-0.8 cursoir-pointer'/>
+             <div className='w-5 h-5 rounder-full bg-cartNumbg flex items-center justify-center'></div>
+             <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+             <p className='text-xs text-white font-semibold'>2</p>
+             </div>
+            </div>
         </div>
 
 
---
+++ src/components/const.js
@@ -0,0 +1,43 @@
+
+export const headerMenu = [
+{
+title: 'Home',
+link: '/home'
+},
+{
+title: 'About',
+link: '/about',
+submenu: [
+{
+title: 'Company',
+link: '/about/company'
+},
+{
+title: 'Team',
+link: '/about/team'
+}
+]
+},
+{
+title: 'Services',
+link: '/services',
+submenu: [
+{
+title: 'Web Development',
+link: '/services/web-development'
+},
+{
+title: 'Mobile Development',
+link: '/services/mobile-development'
+}
+]
+},
+{
+title: 'Contact',
+link: '/contact'
+}
+];
+
+
+
+
